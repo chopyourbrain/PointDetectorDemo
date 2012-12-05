845a68a958e4c7f51d8adf8f7a236fb557b8820d
Franz <info@Locked.de>
2012-12-05 21:40:01 +0100

    init

---
+++ src/main/java/de/lmu/dbs/ifi/jfeaturelib/examples/HaralickExample.java
@@ -0,0 +1,33 @@
+package de.lmu.dbs.ifi.jfeaturelib.examples;
+
+import de.lmu.ifi.dbs.jfeaturelib.features.Haralick;
+import de.lmu.ifi.dbs.utilities.Arrays2;
+import ij.process.ColorProcessor;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import javax.imageio.ImageIO;
+
+public class HaralickExample {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        // load the image
+        File f = new File(HaralickExample.class.getResource("/test.jpg").toURI());
+        ColorProcessor image = new ColorProcessor(ImageIO.read(f));
+        
+        // initialize the descriptor
+        Haralick descriptor = new Haralick();
+        
+        // run the descriptor and extract the features
+        descriptor.run(image);
+        
+        // obtain the features
+        List<double[]> features = descriptor.getFeatures();
+        
+        // print the features to system out
+        for (double[] feature : features) {
+            System.out.println(Arrays2.join(feature, ", ", "%.5f"));
+        }
+    }
+}
